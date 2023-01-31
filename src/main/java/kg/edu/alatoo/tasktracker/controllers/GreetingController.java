cbc8accc6d2475c8cbd2f274c11aad67126975ae
Student06-teen <nuradilovaibek@gmail.com>
2023-01-31 15:53:34 +0600

    Initial commit

---
+++ src/main/java/kg/edu/alatoo/tasktracker/controllers/GreetingController.java
@@ -0,0 +1,18 @@
+package kg.edu.alatoo.tasktracker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required = false,
+            defaultValue = "Noname") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
